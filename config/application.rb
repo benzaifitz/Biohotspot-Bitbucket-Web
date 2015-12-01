ef1b9253d73c88540dd2a6a85a83499f1cae88d1
Hasham Malik <hasham2@gmail.com>
2015-12-01 11:13:46 +0500

    just updated some configuration

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,15 @@ module Fram
   class Application < Rails::Application
 
     config.generators do |g|
+      g.helper false
+      g.javascript_engine false
+      g.stylesheets false
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
-        controller_specs: false,
+        controller_specs: true,
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
