12659b8974d1faae89ec196b01034f938208521c
Bilal <bilal@dapperapps.com.au>
2015-12-08 11:01:19 +0500

    [FRAM-19] Administrators will be able to view details of a terms and conditions version

--- app/admin/eula.rb
+++ app/admin/eula.rb
@@ -3,10 +3,21 @@ ActiveAdmin.register Eula, as: 'Terms And Condition' do
   permit_params :is_latest, :eula_text
 
   form do |f|
-    inputs 'Terms and Conditions' do
+    inputs 'Terms and Conditions (EULA)' do
       input :eula_text, as: :ckeditor, label: "Terms and Condition Text"
       input :is_latest
     end
     actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row (:eula_text) {|eula| raw eula.eula_text}
+      row :is_latest
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
   end
 end
