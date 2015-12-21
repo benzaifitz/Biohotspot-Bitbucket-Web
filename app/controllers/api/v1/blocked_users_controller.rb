e27a363fbcbf8cf19e2d0336f6e8726486b46306
Zahid Ali <zahid@dapperapps.com.au>
2015-12-21 16:24:09 +0500

    [FRAM-182] Create integration and unit tests.
    Integration test for blocked user controller.

--- app/controllers/api/v1/blocked_users_controller.rb
+++ app/controllers/api/v1/blocked_users_controller.rb
@@ -13,13 +13,13 @@ module Api
 
       # GET /api/v1/customers/1.json
       api :GET, '/blocked_users/:id.json', 'Returns user_id and blocked_by_id'
-      param :id, Integer, desc: 'ID of blocked user to be shown.', required: true
+      # param :id, Integer, desc: 'ID of blocked user to be shown.', required: true
       def show
       end
 
       # POST /api/v1/blocked_users.json
       api :POST, '/blocked_users.json', 'Create single record of blocked user.'
-      param :user_id, Integer, desc: 'ID of users who is to be blocked.', required: true
+      # param :user_id, Integer, desc: 'ID of users who is to be blocked.', required: true
       def create
         @blocked_user = BlockedUser.new(blocked_user_params.merge(blocked_by: current_user))
         if @blocked_user.save
@@ -29,8 +29,8 @@ module Api
         end
       end
 
-      api :POST, '/un_blocked_user.json', 'Un blocks the user.'
-      param :user_id, Integer, desc: 'ID of user who is to be un blocked.', required: true
+      api :DELETE, '/un_blocked_user.json', 'Un blocks the user.'
+      # param :user_id, Integer, desc: 'ID of user who is to be un blocked.', required: true
       def destroy
         @blocked_user = BlockedUser.where(blocked_user_params.merge(blocked_by: current_user))
         @blocked_user.destroy_all
