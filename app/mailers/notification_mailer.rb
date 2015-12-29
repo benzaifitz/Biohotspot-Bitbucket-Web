e109b2320c50b57989bc3844b82f9fd243d427bc
zahid_applabs <zahid@dapperapps.com.au>
2015-12-29 11:42:31 +0000

    notification_mailer.rb edited online with Bitbucket

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,7 +4,6 @@ class NotificationMailer < ApplicationMailer
     sender = notification.sender
     recipient = notification.user
     @message = notification.alert
-
     mail(from: sender.email, to: recipient.email, subject: notification.category)
   end
 
@@ -13,7 +12,6 @@ class NotificationMailer < ApplicationMailer
   # from can be skipped if you want to use the default from address for the mailer
   def send_email(options)
     @message = options[:message]
-
     mail(options)
   end
 end
