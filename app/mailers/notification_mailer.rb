997a9958fcdd2c191689f946665e21a9817edfad
Bilal <bilal@dapperapps.com.au>
2015-12-29 16:26:41 +0500

    [FRAM-99]-general method to send email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,4 +7,13 @@ class NotificationMailer < ApplicationMailer
 
     mail(from: sender.email, to: recipient.email, subject: notification.category)
   end
+
+  # provide to, from, subject and message in options
+  # options = {to: 'to@test.com', from: 'from@test.com', subject: 'Hello World!', message: 'Viva la revolucion!'}
+  # from can be skipped if you want to use the default from address for the mailer
+  def send_email(options)
+    @message = options[:message]
+
+    mail(options)
+  end
 end
