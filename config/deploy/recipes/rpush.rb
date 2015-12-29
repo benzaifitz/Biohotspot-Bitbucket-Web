1a27f1983cf7abdb8cc240ac4f4dbe5b80f01f05
Zahid Ali <zahid@dapperapps.com.au>
2015-12-29 16:31:38 +0500

    [FRAM-190] Setup Rpush and redis on production server.
    Remove debugging lines.

--- config/deploy/recipes/rpush.rb
+++ config/deploy/recipes/rpush.rb
@@ -4,10 +4,8 @@ namespace :deploy do
       within release_path do
         with rails_env: fetch(:rails_env) do
           if File.exist?("#{shared_path}/tmp/rpush.pid")
-            puts "===================1"
             execute :bundle, :exec, "rpush stop -e #{fetch(:rails_env)}"
           end
-          puts "===================2"
           execute :bundle, :exec, "rpush start -e #{fetch(:rails_env)}"
         end
       end
