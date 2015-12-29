82c12acc9c6e6fa4ecf8bfe983303631b120ae3b
Zahid Ali <zahid@dapperapps.com.au>
2015-12-29 15:25:14 +0500

    [FRAM-190] Setup Rpush and redis on production server.
    Setup rpush restart.

--- config/deploy/recipes/rpush.rb
+++ config/deploy/recipes/rpush.rb
@@ -1,10 +1,13 @@
-after :finished, :restart_rpush do
-  on roles(:web) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        execute :bundle, :exec, "rpush stop -e #{fetch(:rails_env)}"
-        execute :bundle, :exec, "rpush start -e #{fetch(:rails_env)}"
+namespace :rpush do
+  task :restart do
+    on roles(:web) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, :exec, "rpush stop -e #{fetch(:rails_env)}"
+          execute :bundle, :exec, "rpush start -e #{fetch(:rails_env)}"
+        end
       end
     end
   end
+  after 'deploy:restart', 'rpush:restart'
 end
