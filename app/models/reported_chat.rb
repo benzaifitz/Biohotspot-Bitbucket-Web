c794889fbe6a593a4430c374d8570620c57827d8
Zahid Ali <zahid@dapperapps.com.au>
2016-01-01 17:15:57 +0500

    [FRAM-54] API to report a chat message (CHAT_MessageReport)

--- app/models/reported_chat.rb
+++ app/models/reported_chat.rb
@@ -12,4 +12,15 @@
 class ReportedChat < ActiveRecord::Base
   belongs_to :chat
   belongs_to :reported_by, class_name: "User", foreign_key: "reported_by_id"
+
+  delegate :ban_with_comment, :enable_with_comment, :bannable, to: :user
+
+  validates_presence_of :chat_id, :reported_by_id
+  validates_uniqueness_of :chat_id, :scope => :reported_by_id
+
+  after_create :update_chat_status_to_reported
+
+  def update_chat_status_to_reported
+    Chat.find(self.chat_id).reported!
+  end
 end
