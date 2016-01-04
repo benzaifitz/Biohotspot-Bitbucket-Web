640b0033b5313da2e8cf0859f9dcad3dfa761746
Bilal <bilal@dapperapps.com.au>
2016-01-04 10:03:38 +0500

    [FRAM-191] reverted email config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
     port: 587,
-    # domain: Rails.application.secrets.domain_name,
+    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
