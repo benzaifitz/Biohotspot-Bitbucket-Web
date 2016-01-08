19e24527f47f3cd510acfcd97a0e55227d5783ec
Bilal <bilal@dapperapps.com.au>
2016-01-08 16:49:45 +0500

    [FRAM-191] emails config added to send emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
+  #TODO change this to a valid email once we have a domain
+  config.mailer_sender = 'no-reply@framework.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
