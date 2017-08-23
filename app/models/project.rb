556d7517b9f4d7bc376e10cb38d26f318ebb6dc1
Adnan Ali Arshad <adnan@dapperapps.com.au>
2017-08-23 11:54:48 +0500

    Fix project creation issues.

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,9 @@ class Project < ApplicationRecord
   # belongs_to :project_manager, class_name: 'User'#, foreign_key: :project_manager_id
   serialize :tags
 
-  validates_presence_of :project_manager
+  attr_accessor :project_manager_id
+
+  # validates_presence_of :project_manager
   accepts_nested_attributes_for :project_manager, allow_destroy: true
+
 end
