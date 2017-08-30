f4214a5b18d780e62dd01933fa624304e29d43fa
AliRazaKhan <ali@dapperapps.com.au>
2017-08-30 11:39:40 +0500

    [PWM-47] Add Image size validation of max 2 mb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :imageable, polymorphic: true
   mount_uploader :file, PhotoUploader
+  validates :file,
+            :file_size => {
+                :maximum => 2.0.megabytes.to_i
+            }
   # belongs_to :category
 end
