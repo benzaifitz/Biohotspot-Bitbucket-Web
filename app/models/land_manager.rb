f5d932602afa95215526a7eb5e014349828292f9
Dil Muhammad <dil@dapperapps.com.au>
2017-08-31 12:25:44 +0500

    [#PWM-72] Sends push notification to Land manager on beginning of every month.

--- app/models/land_manager.rb
+++ app/models/land_manager.rb
@@ -1,4 +1,5 @@
 class LandManager < User
+  include PushNotification
   default_scope -> { where(user_type: User.user_types['land_manager']) }
   before_update :check_duplicate_email
 
@@ -7,6 +8,19 @@ class LandManager < User
       self.errors.add(:email, 'Duplicate email!') && false
     else
       true
+    end
+  end
+  def self.send_push_notification_to_land_manager
+    LandManager.all.each do |land_manager|
+      PushNotification.sends(
+          device_type: land_manager.device_type,
+          device_token: land_manager.device_token,
+          #badge: receiver.ios? ? Conversation.get_receivers_unread_conversation_count(receiver.id) : 0,
+          data: {
+              type: 'alert',
+              alert: 'push notification text will be replaced here'
+          }
+      )
     end
   end
 end
