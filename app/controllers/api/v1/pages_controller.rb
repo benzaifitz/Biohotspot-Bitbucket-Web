dc31e76545ac669915a182c0df1181af2813d491
AliRazaKhan <ali@dapperapps.com.au>
2017-09-06 12:04:39 +0500

    [PWM-48] add protocol i.e http:// with port number

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -7,7 +7,7 @@ module Api
       api :GET, '/land_managers/about.json', 'Show about page.'
       # param :id, Integer, desc: 'ID of land_manager to be shown.', required: true
       def about
-        @documents = Document.all.map{|a| {id: a.id, name: a.name, document: "#{request.host_with_port}#{a.document.url}", project: a.try(:project).try(:title), document_category: a.try(:category_document).try(:name)}}
+        @documents = Document.all.map{|a| {id: a.id, name: a.name, document: "#{request.protocol}#{request.host_with_port}#{a.document.url}", project: a.try(:project).try(:title), document_category: a.try(:category_document).try(:name)}}
         @category_documents = CategoryDocument.all
       end
 
