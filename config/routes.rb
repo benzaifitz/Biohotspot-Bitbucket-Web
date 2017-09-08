f945de4a7f33ae9d14d2e277832d5b4cbc0753a1
Zahid Ali <zahid@dapperapps.com.au>
2017-09-08 12:04:35 +0500

    [PWM-4] An administrator will be able to reset their password if they have forgotten it

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   devise_for :users, {
       path: ActiveAdmin.application.default_namespace || "/",
       controllers: {
-        sessions: "users/sessions"
+          passwords: "active_admin/devise/passwords",
+          confirmations: 'active_admin/devise/confirmations',
+          sessions: "users/sessions"
       },
       path_names: {sign_in: 'login', sign_out: "logout"},
       sign_out_via: [*::Devise.sign_out_via, ActiveAdmin.application.logout_link_method].uniq
@@ -15,6 +17,8 @@ Rails.application.routes.draw do
   devise_for :project_managers , {
       path: :pm,
       controllers: {
+          passwords: "active_admin/devise/passwords",
+          confirmations: 'active_admin/devise/confirmations',
           sessions: "active_admin/devise/sessions"
       },
       path_names: { sign_in: 'login', sign_out: "logout"},
@@ -51,7 +55,7 @@ Rails.application.routes.draw do
        resources :submissions
      end
    end
-  ActiveAdmin.routes(self)   
+  ActiveAdmin.routes(self)
   root 'admin/dashboard#index'
   resources :users
 end
