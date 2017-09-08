de7569cbb4eb31e95b570650300b701ff7e6c244
Zahid Ali <zahid@dapperapps.com.au>
2017-09-08 14:32:50 +0500

    [PWM-12] An administrator will be able to approve or reject a user
    Updated API to handle case when user is not approved.

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -7,22 +7,23 @@ module Api
           # @resource will have been set by set_user_by_token concern
           user = User.find_by(email: params[:email])
           user_by_mobile = User.find_by(mobile_number: params[:email]) if params[:email].length > 0
-          if user && user.active?
+          if user && user.active_for_authentication?
             super
           elsif user_by_mobile && user_by_mobile.active? && user_by_mobile.land_manager?
             params[:email] = user_by_mobile.email
             super
           else
-            render_error_banned_user
+            render_error_banned_user(user)
           end
         end
 
         private
 
-        def render_error_banned_user
+        def render_error_banned_user(user)
+          message = user.banned? ? [I18n.t("devise_token_auth.sessions.disabled_account")] : [I18n.t("devise_token_auth.sessions.unapproved_account")]
           render json: {
               success: false,
-              errors: [I18n.t("devise_token_auth.sessions.disabled_account")]
+              errors: message
           }, status: :forbidden
         end
       end
