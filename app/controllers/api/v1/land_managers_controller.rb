c2533ffebe329e09f9bcb4b0392117fd0b93eab7
Zahid Ali <zahid@dapperapps.com.au>
2017-09-18 15:06:24 +0500

    [PWM-187] Internal API error for Get Notifications API
    Remove privacy acceptance verification.

--- app/controllers/api/v1/land_managers_controller.rb
+++ app/controllers/api/v1/land_managers_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
     class LandManagersController < ApiController
-      # before_action :authenticate_user!
-      # before_action :check_user_eula_and_privacy, except: [:update, :about]
-      # before_action :verify_land_manager, only: [:update]
-      # before_action :set_land_manager, only: [:show, :update]
+      before_action :authenticate_user!
+      before_action :check_user_eula_and_privacy, except: [:update, :about]
+      before_action :verify_land_manager, only: [:update]
+      before_action :set_land_manager, only: [:show, :update]
 
       #POST /
       api :POST, '/auth/', 'Create a new user(project_manager or land_manager). No encapsulation needed'
