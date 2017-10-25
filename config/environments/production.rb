a36b5c3d07a04acecc9bb9623b859af3fd0efb8f
Zahid Ali <zahid@dapperapps.com.au>
2017-10-25 12:57:17 +0500

    Change protocol of email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
       :tls  => ActiveRecord::Type::Boolean.new.cast(ENV['EMAIL_TLS'])
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name, :protocol => 'http' }
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name, :protocol => 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
