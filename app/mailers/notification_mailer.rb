22886e805d35b0db8c4c67c052a66b60db7523ce
Dil Muhammad <dil@dapperapps.com.au>
2018-11-05 17:38:02 +0500

    [BIOH-206] Removes email sending to PMs after account confirmation.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,14 +7,6 @@ class NotificationMailer < ApplicationMailer
     mail(to: recipient.email, subject: notification.category)
   end
 
-  def notify_admin_for_account_approval(account_id)
-    @user = User.find(account_id) rescue nil
-    if @user
-      pm_emails = ProjectManager.all.map(&:email)
-      mail(to: pm_emails.join(','), subject: 'Biohotspot: Approve new user.')
-    end
-  end
-
   # provide to, from, subject and message in options
   # options = {to: 'to@test.com', from: 'from@test.com', subject: 'Hello World!', message: 'Viva la revolucion!'}
   # from can be skipped if you want to use the default from address for the mailer
