63288325cf124651922b5fc3c32347b1c73bd431
Dil Muhammad <dil@dapperapps.com.au>
2018-11-08 10:16:49 +0500

    [BIOH-195] Adds additional fields for PM in specie type.

--- app/pm/specie_type.rb
+++ app/pm/specie_type.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register SpecieType, namespace: :pm do
   menu label: 'Species Types', priority: 6
 
   permit_params do
-    allowed = [:id, :name]
+    allowed = [:id, :name, :phylum, :klass, :order, :superfamily, :family, :genus, :species, :sub_species]
     allowed.uniq
   end
 
@@ -13,13 +13,48 @@ ActiveAdmin.register SpecieType, namespace: :pm do
     selectable_column
     id_column
     column :name
+    column :phylum
+    column 'Class', :klass
+    column :order
+    column :superfamily
+    column :family
+    column :genus
+    column :species
+    column :sub_species
     column :created_at
     actions
   end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs 'Species Type Details' do
+      f.input :name
+      f.input :phylum
+      f.input :klass, label: 'Class'
+      f.input :order
+      f.input :superfamily
+      f.input :family
+      f.input :genus
+      f.input :species
+      f.input :sub_species
+    end
+    f.actions
+  end
+
   show do
     attributes_table do
       row :id
       row :name
+      row :phylum
+      row 'Class' do |r|
+        r.klass
+      end
+      row :order
+      row :superfamily
+      row :family
+      row :genus
+      row :species
+      row :sub_species
       row :created_at
       row :updated_at
     end
