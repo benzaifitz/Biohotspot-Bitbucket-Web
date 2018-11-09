d9a6f6416d1bc3bd0d324bf49bd3da695388602e
Dil Muhammad <dil@dapperapps.com.au>
2018-11-09 11:49:30 +0500

    [BIOH-205] Setup SES email

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -60,7 +60,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              ENV['EMAIL_SERVER_ADDRESS'],
       port:                 ENV['EMAIL_PORT'],
-      domain:               ENV['EMAIL_DOMAIN'],
       user_name:            ENV['EMAIL_USERNAME'],
       password:             ENV['EMAIL_PASSWORD'],
       authentication:       :login,
