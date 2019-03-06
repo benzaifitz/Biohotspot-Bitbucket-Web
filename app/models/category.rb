ec0297a6d4d012f627ce0278ff7769c5f1cbb5a7
Zahid Ali <zahid@dapperapps.com.au>
2019-03-06 13:03:37 +0500

    [BIOH-307] The scientific name for species will be made a required field

--- app/models/category.rb
+++ app/models/category.rb
@@ -22,7 +22,7 @@ class Category < ApplicationRecord
   serialize :tags
   LIMIT = 5
   validates_uniqueness_of :name
-  validates_presence_of :name, :specie_type, :species_scientific
+  validates_presence_of :name, :specie_type, :species_scientific, :family_common
 
   validate do |record|
     record.validate_photo_quota
